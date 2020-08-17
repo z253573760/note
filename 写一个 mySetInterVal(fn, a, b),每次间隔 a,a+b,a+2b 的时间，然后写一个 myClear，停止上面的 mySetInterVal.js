000fcfc81c91dfc341b1b7689b57b1b2ccdc3a97
z253573760 <z253573760@gmail.com>
2020-08-17 17:07:33 +0800

    no message

---
+++ 写一个 mySetInterVal(fn, a, b),每次间隔 a,a+b,a+2b 的时间，然后写一个 myClear，停止上面的 mySetInterVal.js
@@ -0,0 +1,26 @@
+//1.写一个 mySetInterVal(fn, a, b),
+//每次间隔 a,a+b,a+2b 的时间，然后写一个 myClear，停止上面的 mySetInterVal
+
+
+function mySetInterVal(fn, a, b) {
+  let step = 0
+  let timer = null
+  const create = (timeout) => {
+    timer = setTimeout(() => {
+      fn(a + step * b)
+      step += 1
+      create(a + step * b)
+    }, timeout);
+  }
+  create(a + step * b)
+
+  return function myClear() {
+    console.log("结束了")
+    clearTimeout(timer)
+  }
+}
+const myClear = mySetInterVal((val) => console.log(val), 1000, 1000)
+
+setTimeout(() => {
+  myClear()
+}, 10000)
