2db71312fa86aa6494bdd66934226f3dfe02cac1
z253573760 <z253573760@gmail.com>
2020-08-17 14:36:42 +0800

    no message

--- 用户无感刷新token.js
+++ 用户无感刷新token.js
@@ -103,7 +103,7 @@ export default async function handler({
   if (now > lastTime && lock === false) { //判断是否过期和是否有刷新token的动作  如果过期并且还没有开始刷新token 就开始进行刷新token的处理 
     lock = true //先上锁
     refreshTokenPromise = refreshToken() //开始刷新token 并且把刷新token的promise保存到全局
-    await refreshTokenPromise //等待刷新token结束后 把锁关上
+    await refreshTokenPromise //等待刷新token结束后 解锁
     lock = false
   }
   if (lock === true) { //发现有锁 说明当前的token 已经过期 并且去刷新token的异步操作还没结束
@@ -113,3 +113,9 @@ export default async function handler({
   // token 没过期  正常请求
   return request(opts)
 }
+// 场景模拟
+// A, B, C 三个异步请求 都需要刷新token 现在同时请求 
+// 1. A进入handler函数 先上锁 并且把刷新token的promise存到全局
+// 2. 与此同时B,C也跟着进来了发现 现在被锁住了 只能排在全局promise的队列后面,等待刷新token的promise结束
+// 3. A由于在判断中 发现需要刷新token然后,一直在在等待刷新token的promise结束,等拿到最新的token后才继续执行
+// 4. 刷新token的promise结束了,promise 队列中的B,C也跟着执行了
