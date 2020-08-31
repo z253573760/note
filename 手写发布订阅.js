5df4f4852f2a06d99d6189f984c3fa51c77cc721
z253573760 <z253573760@gmail.com>
2020-08-31 09:59:36 +0800

    no message

--- 手写发布订阅.js
+++ 手写发布订阅.js
@@ -1,15 +1,15 @@
-// LOW 
+// LOW
 const eventBus = () => ({
   $on(event, fn) {
     if (!this[event]) {
-      this[event] = []
+      this[event] = [];
     }
-    this[event].push(fn)
+    this[event].push(fn);
   },
   $emit(event, ...args) {
-    if (!this[event]) return
-    const list = [...this[event]]
-    list.forEach(cb => cb(...args));
+    if (!this[event]) return;
+    const list = [...this[event]];
+    list.forEach((cb) => cb(...args));
   },
   // vm.$off( [event, callback] ) // 抄vue官网的解释
   // 用法：
@@ -19,61 +19,56 @@ const eventBus = () => ({
   // 如果同时提供了事件与回调，则只移除这个回调的监听器。
   $off(event, fn) {
     if (!arguments.length) {
-      //其实就是初始化 或者 循环清除掉当前的 event 的 key 
+      //其实就是初始化 或者 循环清除掉当前的 event 的 key
       //这边方便理解 懒得秀 就有lowB方式写
-      Object.keys(this).forEach(item => {
-        if (item === "$on") return
-        if (item === "$off") return
-        if (item === "$emit") return
-        this[item] = []
-      })
-      return
+      Object.keys(this).forEach((item) => {
+        if (item === "$on") return;
+        if (item === "$off") return;
+        if (item === "$emit") return;
+        this[item] = [];
+      });
+      return;
     }
     if (!fn) {
-      this[event] = []
-      return
+      this[event] = [];
+      return;
     }
-    const cbs = this[event]
-    const newCbs = []
+    const cbs = this[event];
+    const newCbs = [];
     for (const cb of cbs) {
-      if (cb !== fn) newCbs.push(cb)
+      if (cb !== fn) newCbs.push(cb);
     }
-    this[event] = newCbs
+    this[event] = newCbs;
   },
-})
+});
 
-
-const vm = eventBus()
+const vm = eventBus();
 
 vm.$on("move", (name) => {
-  console.log(name + '正在move')
-})
+  console.log(name + "正在move");
+});
 const fn1 = (name) => {
-  console.log(name + '正在move2')
-}
-vm.$on("move", fn1)
-vm.$emit("move", "小明")
-vm.$emit("move", "小红")
-vm.$off("move", fn1)
-vm.$emit("move", "小明")
-vm.$emit("move", "小红")
-vm.$emit("move", "小明")
-vm.$emit("move", "小红")
-vm.$emit("move", "小明")
-vm.$emit("move", "小红")
+  console.log(name + "正在move2");
+};
+vm.$on("move", fn1);
+vm.$emit("move", "小明");
+vm.$emit("move", "小红");
+vm.$off("move", fn1);
+vm.$emit("move", "小明");
+vm.$emit("move", "小红");
+vm.$emit("move", "小明");
+vm.$emit("move", "小红");
+vm.$emit("move", "小明");
+vm.$emit("move", "小红");
 
-
-
-const 高阶函数 = (cb) => {
-  return (...args) => {
-    try {
-      const res = cb(...args)
-      return [res, null]
-    } catch (err) {
-      new Aha()
-        .fix(err)
-        .log(err)
-      reutrn[null, err]
-    }
-  }
-}
+// const 高阶函数 = (cb) => {
+//   return (...args) => {
+//     try {
+//       const res = cb(...args);
+//       return [res, null];
+//     } catch (err) {
+//       new Aha().fix(err).log(err);
+//       reutrn[(null, err)];
+//     }
+//   };
+// };
--- 深拷贝.js
+++ 深拷贝.js
@@ -1,98 +1,88 @@
-function clone(target, map = new WeakMap()) {
+function deepClone(target, map = new WeakMap()) {
+  //处理正则
+  if (target instanceof RegExp) {
+    return new RegExp(target);
+  }
+  // 处理Date
+  if (target instanceof Date) {
+    return new Date(target);
+  }
+  // 处理构造函数生成的string
+  if (target instanceof String) {
+    return new String(target);
+  }
+  // 处理构造函数生成的Number
+  if (target instanceof Number) {
+    return new Number(target);
+  }
+  // 处理构造函数生成的Boolean
+  if (target instanceof Boolean) {
+    return new Boolean(target);
+  }
   //数组处理
   if (Array.isArray(target)) {
-    return cloneArray(target, map)
+    return cloneArray(target, map);
   }
   // 一般Object处理
   if (target !== null && typeof target === "object") {
-    return cloneObject(target, map)
+    return cloneObject(target, map);
   }
   if (typeof target === "function") {
-    return cloneFunc(target)
+    return cloneFunc(target);
   }
   //基本类型处理 String,Number 之类
-  return target
+  return target;
 }
 
 // 拷贝数组
 function cloneArray(target, map) {
-  // 处理循环引用
-  const res = map.get(target)
-  if (res) {
-    return res
-  }
-  const cloneTarget = []
+  const cloneTarget = [];
   for (const item of target) {
-    const res = clone(item, map)
-    cloneTarget.push(res)
+    const res = deepClone(item, map);
+    cloneTarget.push(res);
   }
-  map.set(target, cloneTarget)
-  return cloneTarget
+  return cloneTarget;
 }
-
-
 
 // 拷贝对象
 function cloneObject(target, map) {
   // 处理循环引用
-  const res = map.get(target)
-  if (res) {
-    return res
+  const isCopy = map.get(target);
+  console.log("isCopy", isCopy);
+  if (isCopy) {
+    return isCopy;
   }
   const cloneTarget = {};
+  map.set(target, cloneTarget);
   for (const key in target) {
-    cloneTarget[key] = target[key]
+    if (target.hasOwnProperty(key)) {
+      const res = deepClone(target[key], map);
+      cloneTarget[key] = res;
+    }
   }
-  map.set(target, cloneTarget)
   return cloneTarget;
 }
 
-
 // 拷贝函数
 function cloneFunc(target) {
-  return target
+  return target;
 }
 
-
-
-
-
-const a = {
-  fn() {
-    console.log("this", this.b)
+const target = [
+  {
+    a: "a",
+    f() {
+      console.log("f");
+    },
   },
-  b: "b"
-}
+];
+target[0].target = target;
 
-const res = clone(a)
-res.b = "vvvv"
+const res = deepClone(target);
+res[0].a = "1";
+console.log(res === target);
 
-
-
-
-a.fn.call({
-  c: "C"
-})
-
-res.fn.call({
-  c: "C"
-})
-
-
-
-
-
-
-// const target = [{
-//   a: "a",
-//   f() {
-//     console.log("f")
-//   }
-// }]
-// target[0].target = target
-
-// const res = clone(target)
-// // res[0].a = "1"
-// console.log(res === target)
-// console.log('克隆后 res:', res)
-// console.log('克隆前 target:', target)
+res[0].c = 1;
+res[0].target = "target";
+// console.log("克隆前 target:", target);
+console.log("克隆后 res:", res);
