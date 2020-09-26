6c9cbdfe55e1f80bd2588403346290135793ef40
z253573760 <z253573760@gmail.com>
2020-09-26 17:18:45 +0800

    no message

---
+++ Promise并行限制.js
@@ -0,0 +1,40 @@
+// JS实现一个带并发限制的异步调度器Scheduler，保证同时运行的任务最多有两个。
+// 完善下面代码的Scheduler类，使以下程序能够正常输出：
+class Scheduler {
+  constructor(maxCount = 2) {
+    this.maxCount = maxCount
+    this.count = 0
+    this.cache = []
+    this.isGoing = false
+  }
+  add(promiseCreator) {
+    this.cache.push(promiseCreator)
+    this.run()
+  }
+  async run() {
+    if (!this.cache.length) return
+    if (this.count >= this.maxCount) return
+    this.count += 1
+    const cb = this.cache.shift()
+    cb().finally(() => {
+      this.count--
+      this.run()
+    })
+  }
+}
+
+const timeout = time => new Promise(resolve => {
+  setTimeout(resolve, time);
+})
+
+const scheduler = new Scheduler();
+
+const addTask = (time, order) => {
+  scheduler.add(() => timeout(time).then(() => console.log(order)))
+}
+
+addTask(1000, '1');
+addTask(500, '2');
+addTask(300, '3');
+addTask(400, '4');
+// output: 2 3 1 4
--- Vue3响应式.js
+++ Vue3响应式.js
@@ -1,85 +1,85 @@
 function isObject(target) {
-  return typeof target === "object" && target !== null
+  return typeof target === "object" && target !== null;
 }
 /* 建立响应式数据 */
 function reactive(target) {
-  if (!isObject(target)) return target
+  if (!isObject(target)) return target;
   return new Proxy(target, {
     get(target, key, receiver) {
-      const res = Reflect.get(target, key, receiver)
-      track(target, key)
-      return reactive(res)
+      const res = Reflect.get(target, key, receiver);
+      track(target, key);
+      return reactive(res);
     },
     set(target, key, val, receiver) {
-      const oldVal = target[key]
-      const res = Reflect.set(target, key, val, receiver)
-      if (oldVal === val) return res
-      trigger(target, key)
-      return res
+      const oldVal = target[key];
+      const res = Reflect.set(target, key, val, receiver);
+      if (oldVal === val) return res;
+      trigger(target, key);
+      return res;
     },
     deleteProperty(target, key) {
-      const ret = Reflect.deleteProperty(target, key)
-      return ret
+      const ret = Reflect.deleteProperty(target, key);
+      return ret;
     },
-  })
+  });
 }
-const effectStack = []
+const effectStack = [];
 /* 声明响应函数cb(依赖响应式数据) */
 function effect(cb) {
   const effect = function () {
-    effectStack.push(effect)
-    cb()
-    effectStack.pop()
-    return cb
-  }
-  effect()
+    effectStack.push(effect);
+    cb();
+    effectStack.pop();
+    return cb;
+  };
+  effect();
 }
 
-const targetMap = new WeakMap()
+const targetMap = new WeakMap();
 /* 依赖收集：建立 数据&cb 映射关系 */
 function track(target, key) {
   // console.log('依赖收集：建立 数据&cb 映射关系')
-  const effectFn = effectStack[effectStack.length - 1]
-  if (!effectFn) return
-  let depsMap = targetMap.get(target)
+  const effectFn = effectStack[effectStack.length - 1];
+  if (!effectFn) return;
+  let depsMap = targetMap.get(target);
   if (!depsMap) {
-    depsMap = new Map()
-    targetMap.set(target, depsMap)
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
   }
-  let deps = depsMap.get(key)
+  let deps = depsMap.get(key);
   if (!deps) {
-    deps = new Set()
-    depsMap.set(key, deps)
+    deps = new Set();
+    depsMap.set(key, deps);
   }
-  deps.add(effectFn)
+  deps.add(effectFn);
 }
 /* 触发更新：根据映射关系，执行cb */
 function trigger(target, key) {
   //console.log('触发更新：根据映射关系，执行cb ')
-  const depsMap = targetMap.get(target)
+  const depsMap = targetMap.get(target);
   if (depsMap) {
-    const deps = depsMap.get(key)
+    const deps = depsMap.get(key);
     if (deps) {
-      deps.forEach(effect => effect())
+      deps.forEach((effect) => effect());
     }
   }
 }
 const obj = {
   a: "A",
   B: {
-    c: "c"
+    c: "c",
   },
-  list: [1, 2, 3, 45, 8]
-}
-const vm = reactive(obj)
+  list: [1, 2, 3, 45, 8],
+};
+const vm = reactive(obj);
 effect(() => {
-  vm.a
-  vm.B
-  console.log("我是副作用", effectStack)
-})
+  vm.a;
+  vm.B;
+  console.log("我是副作用", effectStack);
+});
 effect(() => {
-  vm.B
-  console.log("我是副作用2", effectStack)
-})
-vm.B = "3212312"
+  vm.B;
+  console.log("我是副作用2", effectStack);
+});
+vm.B = "3212312";
 // console.log(effectStack)
