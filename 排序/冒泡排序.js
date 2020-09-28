b7585ebbd456d0d891308f8daa7697205f064ba9
灿灿 <253573760@qq.com>
2020-09-29 01:02:53 +0800

    no message

--- 排序/冒泡排序.js
+++ 排序/冒泡排序.js
@@ -1,17 +1,16 @@
 const list = [1, 99, 2, 33, 444, 57, 3, 4, 3, 5, 6];
 function sort1(list) {
+  list = [...list];
   for (let i = list.length; i >= 0; i -= 1) {
-    let isSort = true; // 假设给的数组就是有序的 直接break
     for (let j = 1; j < i; j += 1) {
       const prev = list[j - 1];
       const cur = list[j];
       if (prev > cur) {
         [list[j - 1], list[j]] = [list[j], list[j - 1]];
-        isSort = false;
       }
     }
-    if (isSort) break;
   }
+  return list;
 }
-sort1(list);
-console.log(list);
+const res1 = sort1(list);
+console.log(res1);
