a0ae809b92d29767e0341f726ff03b456ff99665
灿灿 <253573760@qq.com>
2020-10-02 14:09:20 +0800

    no message

---
+++ 排序/插入排序.js
@@ -0,0 +1,18 @@
+const list = [1, 99, 2, 33, 444, 57, 3, 4, 3, 5, 6];
+function sort1(list) {
+  list = [...list];
+  for (let i = 1; i < list.length; i += 1) {
+    let cur = i;
+    while (cur >= 1) {
+      const a = list[cur];
+      const b = list[cur - 1];
+      if (a < b) {
+        [list[cur - 1], list[cur]] = [list[cur], list[cur - 1]];
+      }
+      cur -= 1;
+    }
+  }
+  return list;
+}
+const res1 = sort1(list);
+console.log(res1);
