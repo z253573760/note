c9ca8c677314a2013d708546ab5430fdc5358d15
z253573760 <z253573760@gmail.com>
2020-11-19 12:07:23 +0800

    no message

--- 回溯遍历树.js
+++ 回溯遍历树.js
@@ -4,45 +4,52 @@ const data = [
     children: [
       {
         name: "b",
-        children: [
-          { name: "e" },
-          { name: "1", children: [{ name: "xxx" }] },
-          { name: "2" },
-          { name: "3" },
-        ],
+        children: [{ name: "e", children: [], code: "eee" }],
+        code: "bbb",
       },
       {
         name: "c",
-        children: [
-          {
-            name: "f",
-            children: [
-              {
-                name: "a2",
-                children: [
-                  { name: "b2", children: [{ name: "e2" }] },
-                  { name: "c2", children: [{ name: "f2" }] },
-                  { name: "d2", children: [{ name: "g2" }] },
-                ],
-              },
-            ],
-          },
-        ],
+        children: [{ name: "f", children: [], code: "fff" }],
+        code: "ccc",
       },
-      { name: "d", children: [{ name: "g" }] },
+      {
+        name: "d",
+        children: [{ name: "g", children: [], code: "ggg" }],
+        code: "ddd",
+      },
+    ],
+    code: "aaa",
+  },
+  {
+    name: "a2",
+    children: [
+      {
+        name: "b2",
+        children: [{ name: "e2", children: [], code: "e22" }],
+        code: "b22",
+      },
+      {
+        name: "c2",
+        children: [{ name: "f2", children: [], code: "f22" }],
+        code: "c22",
+      },
+      {
+        name: "d2",
+        children: [{ name: "g2", children: [], code: "g22" }],
+        code: "d22",
+      },
     ],
   },
 ];
-
 function pailie(list, cb = (v) => v) {
   const result = [];
   function dfs(data, res = []) {
-    if (!data) {
+    if (!data || !data.length) {
       result.push(cb([...res]));
       return;
     }
     for (const item of data) {
-      res.push(item.name);
+      res.push(item);
       dfs(item.children, res);
       res.pop();
     }
@@ -55,7 +62,12 @@ function pailie(list, cb = (v) => v) {
  * 格式化
  * @param {Array} list //[a,b,c,d]
  */
-const parse = (list) => ({ name: list.join("/") });
+const parse = (list) => {
+  return {
+    ...list[list.length - 1],
+    name: list.map((item) => item.name).join("/"),
+  };
+};
 
 //测试
 const res = pailie(data, parse);
