8fd098e60eb335a2b8834fabac54d7a3db60126a
z253573760 <z253573760@gmail.com>
2020-11-23 17:50:38 +0800

    no message

--- 防抖节流.js
+++ 防抖节流.js
@@ -10,36 +10,34 @@ function throttle(func, delay = 60) {
   };
 }
 
-
-
 // #### 函数防抖 过滤重复验证事件
 // 函数防抖是在最后一次事件后才触发一次函数
 
 function debounce(func, delay = 300, I = null) {
   return (...args) => {
-    clearTimeout(I)
-    I = setTimeout(func.bind(null, ...args), delay)
+    clearTimeout(I);
+    I = setTimeout(func.bind(null, ...args), delay);
     // I = setTimeout((...args) => func(...args), delay)
   };
 }
 
-// #### 既要防抖又要节流   
+// #### 既要防抖又要节流
 // 对事件进行防抖处理 但是如果在设定时间间隔内没触发的话 则先触发一次
 // 其实就是在防抖的机制上加一个判断 判断上次fn执行的时间 如果距离上次执行fn的时间超过设置的时间间隔 就执行fn
 // 场景考虑 消息发布订阅的时候 触发发布太频繁考虑到会频繁更改DOM结构 需要做一次节流  但是又担心不能把最终的结果显示到视图上
-function debounceAndThrottle(fn, delay = 60) {
-  let prev = new Date().getTime()
-  let timer = null
+function debounceAndThrottle(fn, delay = 60, diff = 500) {
+  let prev = new Date().getTime();
+  let timer = null;
   return (...args) => {
-    clearTimeout(timer)
-    const cur = new Date().getTime()
-    if (cur - prev > delay) {
-      prev = cur
-      return fn(...args)
+    clearTimeout(timer);
+    const cur = new Date().getTime();
+    if (cur - prev > diff) {
+      prev = cur;
+      return fn(...args);
     }
     timer = setTimeout(() => {
-      prev = cur
-      fn(...args)
+      prev = cur;
+      fn(...args);
     }, delay);
-  }
+  };
 }
