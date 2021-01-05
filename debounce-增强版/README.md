91321284bad3d9521c6b3a1fa380fdcd393853e1
z253573760 <z253573760@gmail.com>
2021-01-05 13:36:52 +0800

    no message

---
+++ debounce-增强版/README.md
@@ -0,0 +1,65 @@
+# debounce
+
+函数防抖增强版
+
+### 功能说明
+
+###
+
+## 代码演示
+
+### 基本用法
+
+#### 基础防抖
+
+```js
+let count = 1;
+const fn = debouce(console.log, 1000);
+fn(1);
+fn(2);
+fn(3);
+fn(4);
+fn(5);
+fn(6); // 1秒后打印6
+```
+
+#### 取消防抖
+
+```js
+const fn = debouce(console.log, 1000); // fn 执行后会返回一个promise
+const suceessHandler = (res) => {
+  //成功回调
+  console.log("debounce1", debounce.isCancel(res));
+  if (debounce.isCancel(res)) {
+    //判断是否是被手动取消的
+    console.log("我是被手动取消的", res);
+  } else {
+    console.log("完整执行", res);
+  }
+};
+// 失败回调
+const errorHadnler = (err) => console.log("err", err);
+
+fn("a").then(suceessHandler, errorHadnler); //
+fn("b").then(suceessHandler, errorHadnler); // fn("a") 被清除
+fn("c").then(suceessHandler, errorHadnler); // fn("b") 被清除
+fn("d").then(suceessHandler, errorHadnler); // fn("c") 被清除
+setTimeout(() => {
+  fn.cancel("手动取消防抖！！！"); // fn("d") 被手动取消了
+}, 2000);
+```
+
+### 参数
+
+| 参数     | 说明                     | 类型                  | 默认值 |
+| -------- | ------------------------ | --------------------- | ------ |
+| fn       | 防抖的函数               | _Function \| Promise_ | -      |
+| delay    | 延迟的时间(ms)           | \_Number              | - 300  |
+| icCancel | 判断是否是一个取消的结果 | \Function             |        |
+
+### 返回值
+
+| 参数   | 说明               | 类型     | 默认值 |
+| ------ | ------------------ | -------- | ------ |
+| return | 包装过后的防抖函数 | Function |        |
+| cancel | 取消防抖的函数     | Function |        |
---
+++ debounce-增强版/index.js
@@ -0,0 +1,60 @@
+const __v__is__cancel = Symbol("__v__is__cancel");
+export function debounce(fn, delay = 3000, timer = null) {
+  if (typeof fn != "function" && !(fn instanceof Promise)) {
+    throw TypeError("debounce-err:必须是一个函数或者Promise");
+  }
+  debounce.isCancel = (target) => !!target[__v__is__cancel];
+  let resolveHandle;
+  const handler = (...args) => {
+    clearTimeout(timer);
+    return new Promise((reslove, reject) => {
+      resolveHandle = reslove;
+      timer = setTimeout(async () => {
+        try {
+          const res = await fn(...args);
+          reslove(res);
+        } catch (err) {
+          reject(err);
+        }
+      }, delay);
+    });
+  };
+  handler.cancel = (message = "我被取消了") => {
+    clearTimeout(timer);
+    timer = null;
+    resolveHandle({
+      message,
+      [__v__is__cancel]: true,
+    });
+  };
+  return handler;
+}
+//开始简单的测试
+
+let count = 1;
+const fn = debounce(
+  (a) =>
+    new Promise((r, j) =>
+      setTimeout(() => {
+        r(a + count);
+      }, 5000)
+    )
+);
+console.log("debounce", debounce.isCancel);
+
+const suceessHandler = (res) => {
+  console.log("debounce1", debounce.isCancel(res));
+  if (debounce.isCancel(res)) {
+    console.log("我是被手动取消的", res);
+  } else {
+    console.log("完整执行", res);
+  }
+};
+const errorHadnler = (err) => console.log("err", err);
+fn("a").then(suceessHandler, errorHadnler);
+fn("b").then(suceessHandler, errorHadnler);
+fn("c").then(suceessHandler, errorHadnler);
+fn("d").then(suceessHandler, errorHadnler);
+setTimeout(() => {
+  fn.cancel("取消防抖了！！！");
+}, 2000);
