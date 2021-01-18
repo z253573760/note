fc2d72796fc221403cc33f0593a1dca3986d38d6
z253573760 <z253573760@gmail.com>
2021-01-18 12:03:02 +0900

    no message

--- vue-photo/index.js
+++ vue-photo/index.js
@@ -5,26 +5,26 @@ const key = "cc-photo";
 let instance;
 
 function getInstance() {
-  const instance = createApp(Photo);
+  if (instance) {
+    return instance;
+  }
+  const _instance = createApp(Photo);
   const root = document.createElement("div");
   document.body.appendChild(root);
-  return instance.mount(root);
+  instance = _instance.mount(root);
+  return instance;
 }
 
 export const usePhoto = () => {
   return inject(key);
 };
 export const showPhoto = (...args) => {
-  if (!instance) {
-    instance = getInstance();
-  }
+  const instance = getInstance();
   instance.showPhoto(...args);
 };
 
 function photo(params) {
-  if (!instance) {
-    instance = getInstance();
-  }
+  const instance = getInstance();
   if (typeof params === "string") {
     instance.showPhoto([params], 0);
   } else {
@@ -33,7 +33,7 @@ function photo(params) {
 }
 
 photo.install = function (app) {
-  instance = getInstance();
+  const instance = getInstance();
   app.config.globalProperties.$photo = instance;
   app.provide(key, instance);
 };
