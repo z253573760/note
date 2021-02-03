b4a8ea32842d41d214b3a047543a39f6ba230c1f
z253573760 <z253573760@gmail.com>
2021-02-03 09:45:15 +0800

    no message

--- Promise并行限制.js
+++ Promise并行限制.js
@@ -2,39 +2,40 @@
 // 完善下面代码的Scheduler类，使以下程序能够正常输出：
 class Scheduler {
   constructor(maxCount = 2) {
-    this.maxCount = maxCount
-    this.count = 0
-    this.cache = []
-    this.isGoing = false
+    this.maxCount = maxCount;
+    this.count = 0;
+    this.cache = [];
   }
   add(promiseCreator) {
-    this.cache.push(promiseCreator)
-    this.run()
+    this.cache.push(promiseCreator);
+    this.run();
+    return promiseCreator;
   }
   async run() {
-    if (!this.cache.length) return
-    if (this.count >= this.maxCount) return
-    this.count += 1
-    const cb = this.cache.shift()
+    if (!this.cache.length) return;
+    if (this.count >= this.maxCount) return;
+    this.count += 1;
+    const cb = this.cache.shift();
     cb().finally(() => {
-      this.count--
-      this.run()
-    })
+      this.count--;
+      this.run();
+    });
   }
 }
 
-const timeout = time => new Promise(resolve => {
-  setTimeout(resolve, time);
-})
+const timeout = (time) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
 
 const scheduler = new Scheduler();
 
 const addTask = (time, order) => {
-  scheduler.add(() => timeout(time).then(() => console.log(order)))
-}
+  scheduler.add(() => timeout(time).then(() => console.log(order)));
+};
 
-addTask(1000, '1');
-addTask(500, '2');
-addTask(300, '3');
-addTask(400, '4');
+addTask(1000, "1");
+addTask(500, "2");
+addTask(300, "3");
+addTask(400, "4");
 // output: 2 3 1 4
