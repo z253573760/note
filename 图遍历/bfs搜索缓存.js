890c04f6ea6a9b17325dc93496f763b939b68f6e
灿灿 <253573760@qq.com>
2021-05-14 01:08:46 +0800

    no message

--- 图遍历/bfs搜索缓存.js
+++ 图遍历/bfs搜索缓存.js
@@ -28,7 +28,6 @@ const list = [
 function bfs(list, cb, startIndex = 0, res = []) {
   for (let i = startIndex; i < list.length; i += 1) {
     const item = list[i];
-    console.log(item.id);
     res.push(...(item.children || []));
     const result = cb(item, i, list, res);
     if (result) {
@@ -49,20 +48,21 @@ function createSearch(list) {
     res: [],
   };
   return (id) => {
+    // 第一次从缓存查找
     const firstSearchRes = map[id];
     if (firstSearchRes) return res;
+    // 没找到目标 bfs 从上次记录的位置 开始继续查找
     bfs(
-      list,
+      log.list,
       (item, index, list, res) => {
         map[item.id] = item;
-        log.index = index;
-        log.list = list;
-        log.res = res;
+        Object.assign(log, { index, list, res });
         return item.id == id;
       },
       log.index,
       log.res
     );
+    // 第二次从缓存查找
     const secondSearchRes = map[id];
     if (secondSearchRes) return secondSearchRes;
     console.warn("没找到");
